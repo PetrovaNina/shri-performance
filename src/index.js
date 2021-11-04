8d985b06d90a9e94c6e7ec0f70b8292f696b8f5a
Nina Petrova <Nina_Petrova@epam.com>
2021-11-04 23:47:24 +0300

    created metrix

--- src/index.js
+++ src/index.js
@@ -7,28 +7,16 @@ const counter = new Counter();
 counter.init('57C50A26-03F6-4E28-B883-3E1F80AD916F', String(Math.random()).substr(2, 12), 'send test');
 counter.setAdditionalParams({
   env: 'development',
-  platform: parser.getDevice().type,
   browser: parser.getBrowser().name,
   os: parser.getOS().name,
+  platform: parser.getDevice().type,
 });
 
-counter.send('platform', parser.getDevice().type)
-counter.send('browser', parser.getBrowser().name)
-counter.send('os', parser.getOS().name)
 counter.send('connect', performance.timing.connectEnd - performance.timing.connectStart);
 counter.send('ttfb', performance.timing.responseEnd - performance.timing.requestStart);
 
 let timeStart = Date.now();
 
-setTimeout(function () {
-  document.querySelector('.quiz-heading').style.color = 'red';
-  counter.send('heading', Date.now() - timeStart);
-}, Math.random() * 1000 + 500);
-
-document.querySelector('.quiz-area').onhover = function () {
-  let timeStart = Date.now();
-
-  setTimeout(function () {
-    counter.send('quiz', Date.now() - timeStart);
-  }, Math.random() * 1000 + 2000);
+document.querySelector('.quiz-area').onmouseover = function () {
+  counter.send('quiz_start (sec)', Math.floor((Date.now() - timeStart) / 1000));
 }
--- stats/stats.js
+++ stats/stats.js
@@ -1,14 +1,14 @@
 function quantile(arr, q) {
-    const sorted = arr.sort((a, b) => a - b);
-    const pos = (sorted.length - 1) * q;
-    const base = Math.floor(pos);
-    const rest = pos - base;
+	const sorted = arr.sort((a, b) => a - b);
+	const pos = (sorted.length - 1) * q;
+	const base = Math.floor(pos);
+	const rest = pos - base;
 
-    if (sorted[base + 1] !== undefined) {
-        return Math.floor(sorted[base] + rest * (sorted[base + 1] - sorted[base]));
-    } else {
-        return Math.floor(sorted[base]);
-    }
+	if (sorted[base + 1] !== undefined) {
+		return Math.floor(sorted[base] + rest * (sorted[base + 1] - sorted[base]));
+	} else {
+		return Math.floor(sorted[base]);
+	}
 };
 
 function prepareData(result) {
@@ -19,28 +19,90 @@ function prepareData(result) {
 	});
 }
 
+const countableMetrics = ['connect', 'ttfb', 'quiz_start (sec)'];
+
+function getDateRange(fromDate, toDate) {
+	const range = [];
+
+	for (
+		let date = new Date(fromDate);
+		date <= new Date(toDate);
+		date.setDate(date.getDate() + 1)
+	) {
+		range.push(date.toISOString().slice(0, 10));
+	}
+
+	return range;
+};
+
 // TODO: реализовать
-// показать значение метрики за несколько день
-function showMetricByPeriod() {
+// показать значение метрики за несколько дней
+function showMetricByPeriod(data, page, startDate, endDate) {
+	console.log(`
+	All metrics for period from ${startDate} to ${endDate}:`);
+
+	const table = {};
+	let i = 1;
+
+	getDateRange(startDate, endDate).forEach(date => {
+		countableMetrics.forEach(metric => {
+			table[i++] = {
+				metric,
+				date,
+				...addMetricByDate(data, page, metric, date),
+			};
+		})
+	});
+
+	console.table(table);
 }
 
 // показать сессию пользователя
-function showSession() {
+function showSession(data, requestId) {
+	console.log(`
+	All metrics for requestID ${requestId}:`);
+
+	let sessionData = data.filter((item) => item.requestId == requestId);
+
+	let table = {};
+
+	sessionData.forEach(data => {
+		table[data.name] = data.value;
+
+		for (let key in data.additional) {
+			table[key] = data.additional[key];
+		};
+	});
+
+	console.table(table);
 }
 
 // сравнить метрику в разных срезах
-function compareMetric() {
-}
+function compareMetric(data, metricName, metrisLocation, date) {
+	console.log(`
+	Comparing of user ${metricName.toUpperCase()} for ${date ? date : 'all the time'}:`);
 
-// любые другие сценарии, которые считаете полезными
+	let queryData = !date ? data :
+		data.filter(item => item.date == date);
 
+	const table = {
+		'TOTAL (users number)': queryData.length,
+	};
 
-// Пример
+	queryData.forEach(item => {
+		let key = metrisLocation ? item[metrisLocation][metricName] : item[metricName];
+		if (key === undefined) key = 'desktop';
+		table[key] ? table[key]++ : table[key] = 1;
+	});
+
+	console.table(table);
+};
+
 // добавить метрику за выбранный день
 function addMetricByDate(data, page, name, date) {
 	let sampleData = data
-					.filter(item => item.page == page && item.name == name && item.date == date)
-					.map(item => item.value);
+		.filter(item => item.page == page && item.name == name && item.date == date)
+		.map(item => item.value);
 
 	let result = {};
 
@@ -54,17 +116,13 @@ function addMetricByDate(data, page, name, date) {
 }
 // рассчитывает все метрики за день
 function calcMetricsByDate(data, page, date) {
-	console.log(`All metrics for ${date}:`);
+	console.log(`
+	All metrics for ${date}:`);
 
 	let table = {};
-	table.connect = addMetricByDate(data, page, 'connect', date);
-	table.ttfb = addMetricByDate(data, page, 'ttfb', date);
-	table.quiz = addMetricByDate(data, page, 'quiz', date);
-	table.heading = addMetricByDate(data, page, 'heading', date);
-	table.os = addMetricByDate(data, page, 'os', date);
-	table.browser = addMetricByDate(data, page, 'browser', date);
-	table.platform = addMetricByDate(data, page, 'platform', date);
-
+	countableMetrics.forEach(metric => {
+		table[metric] = addMetricByDate(data, page, metric, date);
+	});
 	console.table(table);
 };
 
@@ -73,7 +131,11 @@ fetch('https://shri.yandex/hw/stat/data?counterId=57C50A26-03F6-4E28-B883-3E1F80
 	.then(result => {
 		let data = prepareData(result);
 
-		calcMetricsByDate(data, 'send test', '2021-11-31');
-
-		// добавить свои сценарии, реализовать функции выше
+		calcMetricsByDate(data, 'send test', '2021-11-04');
+		showMetricByPeriod(data, 'send test', '2021-11-03', '2021-11-04')
+		showSession(data, '600321823553');
+		compareMetric(data, 'os', 'additional', '2021-11-03');
+		compareMetric(data, 'platform', 'additional', '2021-11-03');
+		compareMetric(data, 'platform', 'additional');
+		compareMetric(data, 'browser', 'additional', '2021-11-03');
 	});
